5fcee0b84b9f4c3cd440a6caf8e9c48b86df7b6c
David <Daavidng@users.noreply.github.com>
2025-05-18 19:22:15 +0800

    update gitignore

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ attempts = 0
 cost = 0
 
 while True:
-    your_numbers = set(random.sample(range(1, 58-1), 6)) 
     attempts += 1
-    cost += 2.00
-    draw = set(random.sample(range(1, 58-1), 6)) 
+    cost += 286.00
+    your_numbers = set(random.sample(range(1, 58-1), 12)) 
+    # draw = set(random.sample(range(1, 58-1), 6)) 
+    draw = set([4, 24, 49, 51, 52, 55])
     
-    if attempts % 1000000 == 0:
+    if attempts % 1000 == 0:
         print(f"Checkpoint: {attempts} attempts...")
 
-    if your_numbers.issubset(draw):
+    if draw.issubset(your_numbers):
         print(f'{sorted(your_numbers)}')
         print(f"\nYour numbers are a subset of the draw after {attempts} attempts!")
         print(f"Draw was: {sorted(draw)}")
